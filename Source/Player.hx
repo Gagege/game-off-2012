1034bd0b60dc2f74b9b30cc0a2e7c93ec93f1f88
Gagege <gage.herrmann@gmail.com>
2012-10-29 22:56:17 -0500

    Adding drawArm function.

--- Source/Player.hx
+++ Source/Player.hx
@@ -22,17 +22,21 @@ class Player extends Sprite
 	private function drawField(player:Int):Void
 	{
 		var railX:Float = 0.0;
+		side:String = "";
 		if (player == 1)
 		{
 			railX = Lib.current.stage.stageWidth * .20;
+			side = "left";
 		}
 		else
 		{
 			railX = Lib.current.stage.stageWidth * .80;
+			side = "right";
 		}
 		
 		drawRail(railX);
 		
+		drawArm(railX, side);
 	}
 	
 	private function drawRail(railX:Float):Void
@@ -42,5 +46,17 @@ class Player extends Sprite
 		rail.graphics.lineStyle(3.0, 0x000000);
 		rail = DrawHelper.makeLine(rail, railX, 0, railX, Lib.current.stage.stageHeight);
 		addChild(rail);
+	}
+	
+	private function drawArm(railX:Float, side:String):Void 
+	{
+			if (side == "left")
+			{
+				
+			}
+			else if (side == "right")
+			{
+				
+			}
 	}
 }
