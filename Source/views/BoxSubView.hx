837fed0c98d38c7f28fd902494bc1f8ac787f139
Gage Herrmann <gage.herrmann@gmail.com>
2012-11-01 17:24:35 -0500

    Added box colors

--- Source/views/BoxSubView.hx
+++ Source/views/BoxSubView.hx
@@ -3,14 +3,13 @@ package views;
 import models.Resource;
 import nme.display.Sprite;
 
-/**
- * ...
- * @author Gage Herrmann
- */
-
 class BoxSubView extends Sprite
 {
 
+	private var lithiumColor:Int = 0x0045E6;
+	private var plutoniumColor:Int = 0x00E6A1;
+	private var uraniumColor:Int = 0xB8E600;
+	
 	public function new(itemInBox:Resource) 
 	{
 		super();
