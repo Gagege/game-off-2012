f7074e71b54c2f9e2b2ec354097c20d572f4f4ec
Gage Herrmann <gage.herrmann@gmail.com>
2012-11-01 15:50:48 -0500

    Changed getset, removed antis

--- Source/models/Resource.hx
+++ Source/models/Resource.hx
@@ -2,12 +2,11 @@ package models;
 
 class Resource 
 {
-	public var(default, default) name:String;
-	public var(default, default) quantity:Int;
-	public var(default, default) type:ResourceType;
+	public var(default, null) name:String;
+	public var(default, null) quantity:Int;
+	public var(default, null) type:ResourceType;
 
-	public function new(resourceName:String, resourceQuantity:Int,
-		resourceType:ResourceType) 
+	public function new(resourceName:String, resourceQuantity:Int, resourceType:ResourceType):Void
 	{
 		name = resourceName;
 		quantity = resourceQuantity;
@@ -17,10 +16,7 @@ class Resource
 
 enum ResourceType
 {
-	Lithium,
-	Plutonium,
-	Uranium,
-	AntiLithium,
-	AntiPlutonium,
-	AntiUranium
+	Lithium;
+	Plutonium;
+	Uranium;
 }
