d56dc91d053bf1a3e488641277280d4442ef1f08
Gage Herrmann <gage.herrmann@gmail.com>
2012-11-01 15:54:05 -0500

    Created Player model

---
+++ Source/models/Player.hx
@@ -0,0 +1,29 @@
+package models;
+
+class Player 
+{
+
+	public var number(default, null):Int;
+	public var lithium(default, default):Int;
+	public var plutonium(default, default):Int;
+	public var uranium(default, default):Int;
+	
+	public function new(playerNumber:Int) 
+	{
+		number = playerNumber;
+	}
+	
+	public function addResource(resource:Resource):Void 
+	{
+		switch(resource.type)
+		{
+			case ResourceType.Lithium:
+				this.lithium += resource.quantity;
+			case ResourceType.Plutonium:
+				this.plutonium += resource.quantity;
+			case ResourceType.Uranium:
+				this.uranium += resource.quantity;
+		}
+	}
+	
+}
