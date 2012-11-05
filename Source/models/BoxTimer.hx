b2a94cd0fcbf1fb5db4670b1b69c6dae27da654b
Gage Herrmann <gage.herrmann@gmail.com>
2012-11-05 17:01:27 -0600

    Adding BoxTimer

---
+++ Source/models/BoxTimer.hx
@@ -0,0 +1,18 @@
+package models;
+
+import haxe.Timer;
+
+class BoxTimer 
+{
+	var private position1:Resource;
+	var private position2:Resource;
+	var private position3:Resource;
+	
+	var private position1Timer:Timer;
+	
+	public function new() 
+	{
+		
+	}
+	
+}
