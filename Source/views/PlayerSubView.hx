6302acd4ba7a271be1874aacacdb4f1467436c80
Gagege <gage.herrmann@gmail.com>
2012-11-07 22:24:03 -0600

    Adding resource labels to player fields

--- Source/views/PlayerSubView.hx
+++ Source/views/PlayerSubView.hx
@@ -1,10 +1,14 @@
 package views;
 
+import jeash.text.Font;
 import models.Player;
 import models.Resource;
 import nme.display.Sprite;
 import nme.display.GradientType;
 import nme.Lib;
+import nme.text.TextField;
+import nme.Assets;
+import nme.text.TextFormat;
 
 import DrawHelper;
 import controllers.PlayController;
@@ -13,6 +17,11 @@ class PlayerSubView extends Sprite
 {	
 	var robotArm:RobotArmSubView;
 	private var model:Player;
+	private var lithiumDisplay:TextField;
+	private var plutoniumDisplay:TextField;
+	private var uraniumDisplay:TextField;
+	private var font:Font;
+	private var format:TextFormat;
 	
 	public function new(player:Int) 
 	{
@@ -48,6 +57,8 @@ class PlayerSubView extends Sprite
 	
 	private function initialize(player:Int):Void
 	{
+		font = Asset.getFont("_sans");
+		format = new TextFormat (font.fontName, 24, 0xFF0000);
 		drawField(player);
 	}
 	
@@ -63,6 +74,20 @@ class PlayerSubView extends Sprite
 		model = new Player(player);
 		
 		robotArm = new RobotArmSubView(isRight);
+		
 		addChild(robotArm);
+	}
+	
+	private var drawResourceMessage(type:ResourceType):Void
+	{
+		switch(type)
+		{
+			
+		}
+	}
+	
+	private function updateField():Void 
+	{
+		
 	}
 }
