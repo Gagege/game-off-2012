3fc8fe060a3bcea3c80ac5678910d5683fb4b4e2
Gage Herrmann <gage.herrmann@gmail.com>
2012-11-12 16:21:28 -0600

    Made order subview selectable

--- Source/views/OrderSubView.hx
+++ Source/views/OrderSubView.hx
@@ -3,53 +3,79 @@ package views;
 import models.Order;
 import nme.display.Sprite;
 import nme.Lib;
+import nme.text.TextField;
 import nme.text.TextFormat;
 
 class OrderSubView extends Sprite
 {
-	private var order:Order;
+	public var order(default, null):Order;
+	public var selected(default, null):Bool;
+	private var border:Sprite;
+	private var background:Sprite;
 	
-	public function new(assignedOrder:Order) 
+	public function new(assignedOrder:Order, isRight:Bool) 
 	{
 		super();
-		initialize(assignedOrder);
+		initialize(assignedOrder, isRight);
 	}
 	
-	private function drawBackground():Void 
+	public function select(selected:Bool):Void 
 	{
-		var background:Sprite = new Sprite();
+		this.selected = selected;
+		drawBackground(selected);
+		drawBorder(selected);
+	}
+	
+	private function drawBackground(selected:Bool):Void 
+	{
 		background.graphics.clear();
-		background.graphics.beginFill(0xffffff, 0.1);
+		if (selected)
+		{
+			background.graphics.beginFill(0x99ff99, 0.8);
+		}
+		else
+		{
+			background.graphics.beginFill(0xffffff, 0.5);
+		}
 		background.graphics.drawRect(
 			0,
 			0,
 			Lib.current.stage.stageHeight * 0.05,
 			Lib.current.stage.stageWidth * 0.05);
-		addChild(background);
 	}
 	
-	private function drawBorder():Void 
+	private function drawBorder(selected:Bool):Void 
 	{
-		var border:Sprite = new Sprite();
 		border.graphics.clear();
-		border.graphics.lineStyle(.5, 0xFFFFFF, 0.2);
+		if (selected)
+		{
+			border.graphics.lineStyle(.8, 0xFFFFFF, 0.5);
+		}
+		else
+		{
+			border.graphics.lineStyle(.5, 0xFFFFFF, 0.2);
+		}
 		DrawHelper.makeLineRect(
 			border,
 			0,
 			0,
 			Lib.current.stage.stageHeight * 0.05,
 			Lib.current.stage.stageWidth * 0.05);
-		addChild(border);
 	}
 	
-	private function drawTitle():Void 
+	private function drawTitle(isRight:Bool):Void 
 	{
-		var fontSize:Float = Lib.current.stage.stageHeight * 0.01;
+		var fontSize:Float = Lib.current.stage.stageHeight * 0.02;
 		var format:TextFormat = new TextFormat (fontSize, 0xFFFFFF);
 		
 		
-		var displayX = this.width * 0.2;
-		var	displayY = this.height * 0.02;
+		var displayX = this.width + (Lib.current.stage.stageWidth * 0.01);
+		var	displayY = 0;
+		
+		if (isRight)
+		{
+			displayX = -(order.name.length * (Lib.current.stage.stageWidth * 0.006));
+		}
 		
 		var title = new TextField();
 		
@@ -60,18 +86,25 @@ class OrderSubView extends Sprite
 		title.selectable = false;
 		title.embedFonts = true;
 		
-		title.text = model.getFormattedMoney();
+		title.text = order.name;
 		
 		addChild(title);
 	}
 	
-	private function initialize(assignedOrder):Void 
+	private function initialize(assignedOrder:Order, isRight:Bool):Void 
 	{
 		order = assignedOrder;
+		selected = false;
 		
-		drawBackground();
-		drawBorder();
-		drawTitle();
+		background = new Sprite();
+		drawBackground(false);
+		addChild(background);
+		
+		border = new Sprite();
+		drawBorder(false);
+		addChild(border);
+		
+		drawTitle(isRight);
 	}
 	
 }
