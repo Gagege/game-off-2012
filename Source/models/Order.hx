91723f08fe8024228b75c8d3f1aabbb930a0b614
Gagege <gage.herrmann@gmail.com>
2012-11-14 07:31:03 -0600

    added isOrderFulfilled method to Order class

--- Source/models/Order.hx
+++ Source/models/Order.hx
@@ -21,12 +21,33 @@ class Order
 	public static function getRandomOrder():Order 
 	{
 		var amountLithium:Int = Math.round(Math.random() * 13);
-		var amountPlutonium:Int = Math.round(Math.random() * 8);
-		var amountUranium:Int = Math.round(Math.random() * 4);
+		var amountPlutonium:Int = Math.round(Math.random() * 6);
+		var amountUranium:Int = Math.round(Math.random() * 2);
 		
 		var worth:Int = (amountLithium * 10) + (amountPlutonium * 50) + (amountUranium * 100);
 		
 		return new Order("Test Inc.", amountLithium, amountPlutonium, amountUranium, worth);
+	}
+	
+	public function isOrderFulfilled(amountLithium:Int, amountPlutonium:Int, amountUranium:Int):Bool
+	{
+		var fulfilled:Bool = false;
+		
+		if (amountLithium >= lithium &&
+			amountPlutonium >= plutonium &&
+			amountUranium >= uranium)
+		{
+			fulfilled = true;
+		}
+		
+		if (fulfilled)
+		{
+			money -= (amountLithium - lithium) * 10;
+			money -= (amountPlutonium - plutonium) * 50;
+			money -= (amountUranium - uranium) * 100;
+		}
+		
+		return fulfilled;
 	}
 	
 }
